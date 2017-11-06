475feb1749595f7c7b03d377f2f8e689bb8283f6
yintengxiang <yintx@566560.cn>
2017-11-06 15:57:40 +0800

    1.1.1

--- TestA/TestA/TestAViewController.m
+++ TestA/TestA/TestAViewController.m
@@ -17,11 +17,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIButton *button = [UIButton buttonWithType:0];
+    button.backgroundColor = [UIColor blackColor];
+    button.frame = CGRectMake(0, 0, 100, 100);
+    [button addTarget:self action:@selector(goBock) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    
+    UIButton *button1 = [UIButton buttonWithType:0];
+    button1.backgroundColor = [UIColor blackColor];
+    button1.frame = CGRectMake(0, 200, 100, 100);
+    [button1 addTarget:self action:@selector(goBock1) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button1];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)goBock
+{
+    [self dismissViewControllerAnimated:YES completion:^{}];
+}
+
+- (void)goBock1
+{
+    
 }
 
 - (void)testShow
