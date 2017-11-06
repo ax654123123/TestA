afb611a1292c1cee4f6c9bfc04031ed862096f4b
yintengxiang <yintx@566560.cn>
2017-11-06 18:24:35 +0800

    nil

--- TestA/TestA/TestAViewController.m
+++ TestA/TestA/TestAViewController.m
@@ -27,16 +27,28 @@
     
     UIButton *button = [UIButton buttonWithType:0];
     button.backgroundColor = [UIColor blackColor];
-    button.frame = CGRectMake(0, 0, 100, 100);
+    button.frame = CGRectMake(0, 100, 100, 100);
+    [button setTitle:@"返回" forState:UIControlStateNormal];
     [button addTarget:self action:@selector(goBock) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
     
     
     UIButton *button1 = [UIButton buttonWithType:0];
     button1.backgroundColor = [UIColor blackColor];
-    button1.frame = CGRectMake(0, 200, 100, 100);
+    button1.frame = CGRectMake(0, 250, 100, 100);
+    [button1 setTitle:@"push B" forState:UIControlStateNormal];
+
     [button1 addTarget:self action:@selector(goBViewController) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button1];
+    
+    
+    UIButton *button2 = [UIButton buttonWithType:0];
+    button2.backgroundColor = [UIColor blackColor];
+    button2.frame = CGRectMake(200, 300, 100, 100);
+    [button2 setTitle:@"调用B函数" forState:UIControlStateNormal];
+
+    [button2 addTarget:self action:@selector(showB) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button2];
     
 }
 
@@ -64,6 +76,11 @@
 {
    UIViewController *controller =  [[CTMediator sharedInstance] viewControllerTestB];
     [self.navigationController pushViewController:controller animated:YES];
+}
+
+- (void)showB
+{
+    [[CTMediator sharedInstance] tsetBShowAlert];
 }
 
 - (void)testShow
